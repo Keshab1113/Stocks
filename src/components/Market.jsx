77ceb0397585294c7c385123898a0a9934662b1b
Swadesh Pal <swadeshpal2002@gmail.com>
2023-10-26 23:24:14 +0530

    bug fixed

--- src/components/Market.jsx
+++ src/components/Market.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import { BiChevronDown } from "react-icons/bi";
-import { AiOutlinePlusCircle } from "react-icons/Ai";
+import { BsPlusCircle } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 const Market = () => {
@@ -14,7 +14,7 @@ const Market = () => {
             <Link to="/sharedata" className=" bg-slate-300 h-24 w-1/4 rounded-xl p-6">
               <h1>NIFTY 50</h1>
               <h2>19996.35 176.40(0.89%)</h2>
-              </Link>
+            </Link>
             <Link to="/sharedata" className=" bg-slate-300 h-24 w-1/4 rounded-xl p-6">
               <h1>SENSEX</h1>
               <h2>19996.35 176.40(0.89%)</h2>
@@ -57,24 +57,24 @@ const Market = () => {
               </Link>
             </div>
           </div>
-          
+
         </div>
         <div className=" w-3/12">
-            <h1 className=" w-11/12  border-b mt-7 text-2xl font-semibold font-serif text-white">
-              All Watchlists
+          <h1 className=" w-11/12  border-b mt-7 text-2xl font-semibold font-serif text-white">
+            All Watchlists
+          </h1>
+          <div className=" bg-slate-300 h-52 w-11/12 mt-4 rounded-xl p-6">
+            <h1 className="flex justify-between mt-4">
+              Your Watchlist
+              <BiChevronDown className=" text-3xl cursor-pointer" />
             </h1>
-            <div className=" bg-slate-300 h-52 w-11/12 mt-4 rounded-xl p-6">
-              <h1 className="flex justify-between mt-4">
-                Your Watchlist
-                <BiChevronDown className=" text-3xl cursor-pointer" />
-              </h1>
-              <h2>50 items</h2>
-              <div className=" bg-slate-400 h-8 w-full rounded flex justify-center items-center mt-10">
-                <AiOutlinePlusCircle className=" text-2xl mr-2" />
-                Create New Watchlist
-              </div>
+            <h2>50 items</h2>
+            <div className=" bg-slate-400 h-8 w-full rounded flex justify-center items-center mt-10 py-2">
+              <BsPlusCircle className=" text-xl mr-2" />
+              Create New Watchlist
             </div>
           </div>
+        </div>
       </div>
     </div>
   );
